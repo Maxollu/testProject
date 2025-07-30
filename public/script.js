5de5225f05342739e344661b87f4bd508bc71a57
Lupsha Max <lupshamax73@gmail.com>
2025-07-30 20:18:43 +0300

    update 8 (Signup page + delete user btn)

---
+++ public/script.js
@@ -0,0 +1,252 @@
+const searchForm = document.getElementById('searchForm');
+const searchUsername = document.getElementById('searchUsername');
+const searchDiv = document.getElementById('searchResult');
+const tableUl = document.getElementById('usersTable');
+
+(async () => {
+    const email = localStorage.getItem('authEmail');
+    const password = localStorage.getItem('authPassword');
+
+    if (!email || !password) {
+        console.log('AuthEmail:', email);
+        console.log('AuthPassword:', password);
+        window.location.href = 'signup.html'; // або login.html
+        return;
+    }
+
+    // Перевіряємо авторизацію на сервері
+    try {
+        const response = await fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        });
+
+        if (!response.ok) {
+            throw new Error('Unauthorized');
+        }
+
+        // optionally: const user = await response.json();
+        console.log('Авторизація пройдена');
+    } catch (err) {
+        localStorage.removeItem('authEmail');
+        localStorage.removeItem('authPassword');
+        window.location.href = 'signup.html';
+    }
+})();
+
+function logout() {
+    localStorage.removeItem('authEmail');
+    localStorage.removeItem('authPassword');
+    window.location.href = 'signup.html';
+}
+
+async function fetchData() {
+    const response = await fetch('http://localhost:3000/api/users'); // Await the fetch call
+    const data = await response.json(); // Await the parsing of the JSON data
+    console.log(data);
+}
+
+async function searchUser(username){
+    const response = await fetch('http://localhost:3000/api/users/search', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: username
+        })
+    })
+    const data = await response.json();
+    return {ok: response.ok, data};
+}
+
+async function usersTable() {
+    const response = await fetch('http://localhost:3000/api/users/table', {
+        method: 'GET'
+    });
+    const data = await response.json();
+    return {ok: response.ok, data};
+}
+
+function createUserListItem(item) {
+    const li = document.createElement('li');
+    li.style.marginBottom = '20px';
+
+    const userInfo = document.createElement('div');
+    userInfo.classList.add('user-info');
+    userInfo.innerHTML = `
+    <p><strong>Username:</strong> <span class="user-username">${item.username}</span></p>
+    <p><strong>Email:</strong> <span class="user-email">${item.email}</span></p>
+    <p><strong>ID:</strong> <span class="user-id">${item.id}</span></p>
+    `;
+
+    li.appendChild(userInfo);
+
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Редагувати';
+    li.appendChild(editButton);
+
+    const dialog = document.createElement('dialog');
+
+    const title = document.createElement('h3');
+    title.textContent = 'Редагування користувача';
+
+    const usernameLabel = document.createElement('label');
+    usernameLabel.textContent = 'Змінити username: ';
+
+    const usernameInput = document.createElement('input');
+    usernameInput.value = item.username;
+
+    const emailLabel = document.createElement('label');
+    emailLabel.textContent = 'Змінити email: ';
+
+    const emailInput = document.createElement('input');
+    emailInput.value = item.email;
+
+    const submitButton = document.createElement('button');
+    submitButton.textContent = 'Змінити';
+
+    const closeButton = document.createElement('button');
+    closeButton.textContent = 'Закрити';
+    closeButton.style.marginLeft = '10px';
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Видалити користувача';
+    deleteButton.style.marginLeft = '20px';
+
+    dialog.appendChild(title);
+    dialog.appendChild(usernameLabel);
+    dialog.appendChild(usernameInput);
+    dialog.appendChild(document.createElement('br'));
+
+    dialog.appendChild(emailLabel);
+    dialog.appendChild(emailInput);
+    dialog.appendChild(document.createElement('br'));
+
+    dialog.appendChild(submitButton);
+    dialog.appendChild(closeButton);
+    dialog.appendChild(deleteButton);
+
+    editButton.addEventListener('click', () => {
+        dialog.showModal();
+    });
+
+    closeButton.addEventListener('click', () => {
+        dialog.close();
+    });
+
+    deleteButton.addEventListener('click', async () => {
+        try {
+            const response = await fetch(`http://localhost:3000/api/users/${item.id}`, {
+                method: 'DELETE'
+            })
+
+            const data = await response.json();
+
+            if (!response.ok) {
+                throw new Error(data.msg || 'Delete failed');
+            }
+            dialog.close()
+            updateTable();
+            console.log('Deleted user: ', data.user);
+        } catch (err) {
+            console.log('Помилка: ', err.message);
+        }
+    })
+
+    submitButton.addEventListener('click', async () => {
+        const newUsername = usernameInput.value.trim();
+        const newEmail = emailInput.value.trim();
+
+        if (!/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(newEmail)) {
+            alert("Email некоректний");
+            return;
+        }
+
+        const response = await fetch(`http://localhost:3000/api/users/${item.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username: newUsername, email: newEmail })
+        });
+
+        if (response.ok) {
+            dialog.close();
+            updateTable();
+
+            const currentEmail = localStorage.getItem('authEmail');
+            if (currentEmail === item.email) {
+                localStorage.setItem('authEmail', newEmail);
+                console.log('LocalStorage оновлено: authEmail');
+            }
+        } else {
+            alert('Помилка при оновлені користувача.');
+        }
+    });
+
+    li.appendChild(dialog);
+    return li;
+}
+
+async function updateTable() {
+    tableUl.innerHTML = '';
+
+    try{
+        const response = await usersTable();
+        if (!response.ok) {
+            tableUl.textContent = 'Помилка отримання користувача.';
+            return;
+        }
+        response.data.users.forEach(user => {
+            const li = createUserListItem(user);
+            tableUl.appendChild(li);
+        });
+    } catch (error) {
+        console.error(error);
+        tableUl.textContent = 'Сталася помилка при завантаженні користувачів.'
+    }
+}
+
+updateTable();
+
+searchForm.addEventListener('submit', async function(event) {
+    event.preventDefault();
+    const { ok, data } = await searchUser(searchUsername.value);
+
+    searchDiv.textContent = '';
+    searchDiv.className = '';
+
+    if (ok) {
+        searchDiv.textContent = `Username: ${data.username} exists with ID: ${data.id} and Email: ${data.email}`;
+        searchDiv.classList.add('search-success')
+    } else {
+        searchDiv.textContent = `Error: ${data.msg || 'Unknown error'}`;
+        searchDiv.classList.add('search-error')
+    }
+})
+
+// const email = document.getElementById('email');
+// const password = document.getElementById('password');
+// async function createUser(username) {
+//     const response = await fetch('http://localhost:3000/api/users', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             username: username,
+//             email: email.value,
+//             password: password.value
+//         })
+//     })
+//
+//     const data = await response.json();
+//
+//     if(!response.ok) {
+//         throw new Error(data.msg || "Unknown error")
+//     }
+//
+//     return data;
+// }
---
+++ public/signup.js
@@ -0,0 +1,153 @@
+const authForm = document.getElementById('authForm');
+const formTitle = document.getElementById('formTitle');
+const toggleText = document.getElementById('toggleText');
+const toggleLink = document.getElementById('toggleLink');
+
+let isLogin = true;
+
+function renderForm() {
+    authForm.textContent = '';
+
+    if(isLogin) {
+        formTitle.textContent = 'Login'
+        toggleText.innerHTML = 'Немає аккаунта? <a href="#" id="toggleLink">Зареєструватися</a>';
+
+        authForm.innerHTML = `
+             <label for="email">Email</label>
+             <input type="email" id="email" name="email"/>
+             <div id="emailError" class="error"></div>
+             
+             <label for="password">Password</label>
+             <input type="password" id="password" name="password"/>
+             <div id="passwordError" class="error"></div>
+             
+             <div id="resultDiv"></div>
+             <button id="submitBtn">Надіслати</button>
+        `;
+    } else {
+        formTitle.textContent = "Signup";
+        toggleText.innerHTML = "Вже є аккаунт? <a href=\"#\" id=\"toggleLink\">Увійти</a>";
+
+        authForm.innerHTML = `
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username"/>
+        
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email"/>
+            <div id="emailError" class="error"></div>
+        
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password"/>
+        
+            <label for="password2">Repeat password</label>
+            <input type="password" id="password2" name="password2"/>
+            <div id="passwordError" class="error"></div>
+        
+            <div id="resultDiv"></div>
+            <button id="submitBtn">Надіслати</button>
+        `;
+    }
+
+    document.getElementById('toggleLink').addEventListener('click', (e) => {
+        e.preventDefault();
+        isLogin = !isLogin;
+        renderForm();
+    });
+}
+
+renderForm();
+
+authForm.addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const emailError = document.getElementById('emailError');
+    const passwordError = document.getElementById('passwordError');
+    const resultDiv = document.getElementById('resultDiv');
+
+    resultDiv.textContent = '';
+    emailError.textContent = '';
+    passwordError.textContent = '';
+
+    if(isLogin) {
+        if(!email.value || !password.value) {
+            resultDiv.textContent = 'All fields are required.'
+            return;
+        }
+
+        try {
+            const response = await fetch('http://localhost:3000/login', {
+              method: "POST",
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify( {
+                  email: email.value,
+                  password: password.value
+              })
+            });
+
+            const data = await response.json();
+
+            if(!response.ok) {
+                throw new Error(data.msg || "Login error");
+            }
+            localStorage.setItem('authEmail', email.value);
+            localStorage.setItem('authPassword', password.value);
+
+            window.location.href = 'main.html';
+        } catch (error) {
+            resultDiv.textContent = `Помилка: ${error.message}`
+        }
+
+    } else {
+        const username = document.getElementById('username');
+        const password2 = document.getElementById('password2');
+
+        let valid = true;
+
+        if (!username.value || !email.value || !password.value || !password2.value) {
+            resultDiv.textContent = 'Усі поля обов’язкові!';
+            valid = false;
+        }
+
+        if (password.value.length < 6) {
+            passwordError.textContent = 'Пароль повинен бути мінімум 6 символів.';
+            valid = false;
+        } else if (password.value !== password2.value) {
+            passwordError.textContent = 'Паролі не співпадають.';
+            valid = false;
+        }
+
+        if (!email.value.match(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/)) {
+            emailError.textContent = 'Введи коректний email.';
+            valid = false;
+        }
+
+        if (!valid) return;
+
+        try {
+            const response = await fetch('http://localhost:3000/signup', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    username: username.value,
+                    email: email.value,
+                    password: password.value
+                })
+            });
+
+            const data = await response.json();
+
+            if (!response.ok) {
+                throw new Error(data.msg || 'Signup error');
+            }
+
+            localStorage.setItem('authEmail', email.value);
+            localStorage.setItem('authPassword', password.value);
+
+            window.location.href = 'main.html';
+        } catch (error) {
+            resultDiv.textContent = `Помилка: ${error.message}`;
+        }
+    }
+});
